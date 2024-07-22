f10a2bcb247c74487e82b010c792bc0fb7fec1c2
antoinebtn <antoinebtn@gmail.com>
2024-07-22 14:40:21 +0200

    BookController

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = Book::all();
+
+        return view('books.index', ['books' => $books]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'author' => 'required|max:255',
+            'year' => 'required',
+            'genre' => 'max:255'
+        ]);
+
+        Book::create($validatedData);
+
+        return redirect('/books')->with('success', 'Book created!');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Book $book)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $id)
+    {
+        $book = Book::findOrfail($id);
+
+        $book->each->delete();
+
+        return redirect('/books')->with('success', 'Book deleted!');
+    }
+}
